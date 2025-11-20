1a24e13105495f8b295031d607a325a81421cf4c
Santiago Zapata <dev.santizapata@gmail.com>
2025-11-20 10:20:08 -0500

    Json implementation with functions
    I complete the functions and I made an integration with Json to have the data save

--- sistemaBibliotecaVirtual.py
+++ sistemaBibliotecaVirtual.py
@@ -2,49 +2,101 @@ import json
 
 archivo = "biblioteca.json"
 
-def verLibrosDisponible ():
-    print("Ver libros disponibles")
+def cargarDatos():
+    try:
+        with open (archivo, "r") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        guardarDatos([])
+        return []
+    except json.JSONDecodeError:
+        print ("Se creará un nuevo archivo\n")
+        guardarDatos
+        return []
+   
+def guardarDatos(data):
+    with open (archivo, "w") as f:
+     json.dump (data, f, indent = (4))
 
-def agregarNuevosLibro ():
-    print("Agregar nuevos libros")
+def verLibrosDisponible (lista):
+    if not lista:
+        print ("No hay libros registrados\n")
+    else:
+        print ("--- Lista de libros ---")
+        for i in lista:
+            print (f"Nombre: {i["nombre"]} | Estado: {i["estado"]}")
+    print("")
 
-def prestarLibros():
-    print("Prestar libros")
+def agregarNuevosLibro (lista):
+    nombre = input("Nombre del libro: ") 
+    estado = input("Ingresa el estado (Prestado o Inventario): ")
+    
+    libro = {"nombre" : nombre, "estado" : estado}
 
-def devolverLibros():
-    print("Devolver libros")
+    lista.append(libro)
+    guardarDatos(lista)
+    print("Libro agregado correctamente.\n")
 
-def historial():
-    print("Historial")
+def prestarLibros(lista):
+    verLibrosDisponible(lista)
+    nombreBuscar = input("Nombre del libro a prestar: ")
+
+    for i in lista:
+        if i["nombre"] == nombreBuscar:
+            if i["estado"] != "Prestado":
+                i["estado"] = "Prestado"
+                guardarDatos(lista) 
+                print("Perfecto, recuerda devolverlo.")
+                return
+            else:
+                print("El libro ya esta prestado\n")
+                return
+    print("No se encontró el libro.\n")
+
+def devolverLibros(lista):
+    verLibrosDisponible(lista)
+    nombreBuscar = input("Nombre del libro a devolver: ")
+
+    for i in lista:
+        if i["nombre"] == nombreBuscar:
+            if i["estado"] == "Prestado":
+                i["estado"] = "Inventario"
+                guardarDatos(lista) 
+                print("Perfecto, ya se devolvio.\n")
+                return
+            else:
+                print("El libro esta en inventario\n")
+                return
+    print("No se encontró el libro.\n")
 
 programa = 0
 
-while programa != 6:
+while programa != 5:
     try:
         print("1 - Ver libros disponibles\n" \
         "2 - Agregar nuevos libros\n" \
         "3 - Prestar libro\n" \
         "4 - Devolver libros\n" \
-        "5 - Devolver libros\n" \
-        "6 - Salir")
+        "5 - Salir")
         programa = int(input("Que quieres hacer? "))
     except:
         print("Ingresa un valor valido\n")
+
+    print("")
     
     if programa < 1 or programa > 6:
         print("Ingresa un valor valido\n")
+    
+    lista = cargarDatos()
 
     if programa == 1:
-        verLibrosDisponible()
+        verLibrosDisponible(lista)
 
     if programa == 2:
-        agregarNuevosLibro()
+        agregarNuevosLibro(lista)
 
     if programa == 3:
-        prestarLibros()
+        prestarLibros(lista)
 
     if programa == 4:
-        devolverLibros()
-
-    if programa == 5:
-        historial()
+        devolverLibros(lista)
